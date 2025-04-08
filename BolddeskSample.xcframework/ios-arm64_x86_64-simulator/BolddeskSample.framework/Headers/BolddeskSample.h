94bd1800dc5e9fe09168c44d75a94eb3fb009187
Boomibalan <boomibalan.b@syncfusion.com>
2025-04-09 01:26:45 +0530

    Changes

---
+++ BolddeskSample.xcframework/ios-arm64_x86_64-simulator/BolddeskSample.framework/Headers/BolddeskSample.h
@@ -0,0 +1,18 @@
+//
+//  BolddeskSample.h
+//  BolddeskSample
+//
+//  Created by mohammedismailsameer on 09/04/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BolddeskSample.
+FOUNDATION_EXPORT double BolddeskSampleVersionNumber;
+
+//! Project version string for BolddeskSample.
+FOUNDATION_EXPORT const unsigned char BolddeskSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BolddeskSample/PublicHeader.h>
+
+
